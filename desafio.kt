d661057dee1e81607cceead2a44f659f9cd729e1
Ronan Brito <57158773+ronanbss@users.noreply.github.com>
2022-12-23 11:54:26 -0300

    entrega do desafio bootcamp

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,69 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+data class Usuario(val nameUser: String, val registrationUser: String) {
+    override fun toString(): String {
+        return "$nameUser (matrícula $registrationUser)"
     }
 }
 
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60) {
+    override fun toString(): String {
+        return "$nome (Duração: $duracao)"
+    }
+}
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+
+    val inscritos = mutableListOf<Usuario>()
+    
+    fun matricular(vararg usuario: Usuario) {
+        inscritos.addAll(usuario)
+    }
+    
+    override fun toString(): String {
+        return """
+        Formação: $nome
+        Conteúdo educacional: $conteudos
+        Duração do conteúdo: 
+        Nível: $nivel
+        Alunos inscritos na Formação: $inscritos
+        """.trimIndent()
+    	}
+        
+    }
+
+
+fun generateMatricula(): String {
+    return (1000..10000).random().toString()
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val ronan = Usuario("Ronan", generateMatricula())
+    val pedro = Usuario("Pedro", generateMatricula())
+    val bruna = Usuario("Bruna", generateMatricula())
+    val jonas = Usuario("Jonas", generateMatricula())
+    
+    val conteudoEducacionalJava = ConteudoEducacional("Java", 150)
+    val conteudoEducacionalKotlin = ConteudoEducacional("Kotlin", 120)
+        
+    val formacaoJava =
+            Formacao(
+                	"Java Developer"
+                     , listOf(conteudoEducacionalJava)
+                     , Nivel.INTERMEDIARIO
+            )
+    val formacaoKotlin =
+            Formacao(
+                "Formação Kotlin Developer"
+                , listOf(conteudoEducacionalKotlin)
+                , Nivel.BASICO
+            )
+ 
+    formacaoJava.matricular(pedro, jonas)
+    formacaoKotlin.matricular(ronan, bruna)
+   
+    println(formacaoJava)
+    println("\n**********************\n")
+    println(formacaoKotlin)
+    println("\n**********************\n")
 }
